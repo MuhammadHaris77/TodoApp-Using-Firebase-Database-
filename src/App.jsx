16296eacb4fb34ff1311057c67d79e45f2a9b9b8
muhammad-haris <harisqureshi415@gmail.com>
2024-11-02 05:37:42 -0700

    edit or delete functionality update

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,14 @@
 import { useEffect, useRef, useState } from "react"
 import Todo from "./components/Todo"
-import { collection, addDoc, getDocs} from "firebase/firestore";
+import {updateDoc, collection, addDoc,deleteField, getDocs, deleteDoc,doc} from "firebase/firestore";
 import { db } from "./config/config";
 import { auth } from "./config/config";
 import { query, where } from "firebase/firestore";
 
 function App() {
+ const inputValue = useRef()
+  const [todo, setTodo] = useState([])
+
  
 
   useEffect(() => {
@@ -28,17 +31,15 @@ function App() {
     
   }, [])
 
- const inputValue = useRef()
-  const [todo, setTodo] = useState([])
-
   const addTodo = async (event) => {
     event.preventDefault()
     console.log(inputValue.current.value)
     try {
       const docRef = await addDoc(collection(db, "todos"), {
-        title: inputValue.current.value,
+           
+          title: inputValue.current.value,
         uid: auth.currentUser.uid,
-
+       
       });
 
       console.log("Document written with ID: ", docRef.id);
@@ -57,24 +58,38 @@ function App() {
       console.error("Error adding document: ", e);
     }
 
+    inputValue.current.value === ""
 
+  }
+
+
+
+  const deleteTodo = async ({docid},index) => {
+//  console.log(item.docid)
+         todo.splice(index,1)
+        setTodo([...todo])
+console.log(todo.splice(index,1))
+await deleteDoc(doc(db, "todos", docid));
+   
 
   }
 
+//// update function ////////////
+
+
+const updateTodo = async ({docid},index)=>{
+  const newValue = prompt("enter value?")
+  todo.splice(index,1,{title :newValue } )
+  setTodo([...todo])
+  console.log(todo)
+await updateDoc(doc(db, 'todo', docid), {
+ title:newValue     
+});
+}
 
 
 
 
-
-
-
-
-  const deleteTodo = async (docid) => {
-  console.log(uid)
-
-   await deleteDoc(doc(db, "todos", docid));
-
-  }
 
 
   return (
@@ -82,15 +97,15 @@ function App() {
       <Todo reference={inputValue} func={addTodo} / >
 
       <div className='container  pt-4 mt-4 '>
-        <form style={{ width: '70%', margin: 'auto' }} className="bg-light p-4 shadow-lg p-3 mb-5 bg-body rounded " >
+        <div style={{ width: '70%', margin: 'auto' }} className="bg-light p-4 shadow-lg p-3 mb-5 bg-body rounded " >
           <ol>
-            {todo.length > 0 ? todo.map((item) => {
-              return <li className="bg-dark rounded d-flex bd-highlight p-2 m-1 text-light text-center " key={item.docid}><b className="w-100">{item.title}</b>
+            {todo.length > 0 ? todo.map((item,index) => {
+              return <li className="bg-dark rounded d-flex bd-highlight p-2 m-1 text-light text-center " key={index}><b className="w-100">{item.title}</b>
 
-                <button className="btn btn-outline-success mx-2 "  >
+                <button className="btn btn-outline-success mx-2 " onClick={()=>updateTodo(item,index)}  >
                   Edit
                 </button>
-                <button className="btn btn-outline-danger mx-2" onClick={()=>deleteTodo(docid)}  >
+                <button className="btn btn-outline-danger mx-2" onClick={()=>deleteTodo(item)}  >
                   Delete
                 </button>
               </li>
@@ -98,7 +113,7 @@ function App() {
             }
           </ol>
 
-        </form>
+        </div>
       </div>
 
     </div>
